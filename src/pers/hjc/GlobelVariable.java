8685f75868583ce9c2aab71dae58eeec00a9ff81
hjc <879404301@qq.com>
2018-03-16 15:26:20 +0800

    全局变量

---
+++ src/pers/hjc/GlobelVariable.java
@@ -0,0 +1,26 @@
+package pers.hjc;
+
+public class GlobelVariable
+{
+
+	public static String SESSION_USER_ID = "userID";
+
+	public static Long STUDENT_ROLE = 1L;
+	public static Long TEACHER_ROLE = 2L;
+	public static Long ADMIN_ROLE = 3L;
+
+	public static String APPLICATION_IMAGE_PATH = "images";
+	public static String APPLICATION_IMAGE_SIZE = "imagesNumber";
+
+//	public static final String APPLICATION_USER_PATH = "users";
+//	public static final String APPLICATION_USER_SIZE = "usersNumber";
+	
+	public static final String APPLICATION_TEACH_PATH = "teachings";
+	public static final String APPLICATION_TEACH_SIZE = "teachingsNumber";
+
+	public static final String APPLICATION_PUBLICATION_PATH = "publications";
+	public static final String APPLICATION_PUBLICATION_SIZE = "publicationsNumber";
+
+	public static final String APPLICATION_RESEARCH_PATH = "researchs";
+	public static final String APPLICATION_RESEARCH_SIZE = "researchsNumber";
+}
